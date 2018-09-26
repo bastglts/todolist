98cccc22ee2ef4294c1c9562dc1c8a6806e816ee
Bastien Gili-tos <bast.gilitos@gmail.com>
2018-09-26 17:40:51 +0200

    set pug as template engine

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const bodyParser = require('body-parser');
 /* Use session middleware */
 app.use(cookieSession({ secret: 'secretkey' }));
 
+/* Use pug as template engine */
+app.set('view engine', 'pug');
 
 /* Create application/x-www-form-urlencoded parser */
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
-app.get('/', (req, res) => res.render('index.pug'));
+app.get('/', (req, res) => {
+  if (!req.session.todolist) {
+    req.session.todolist = {};
+  }
+
+  res.render('index');
+});
 
 
 app.listen(8080, () => console.log(`App listening on port 8080!`));

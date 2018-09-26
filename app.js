f402d6a6b2cd585f4f9e9800c0b01a0c54801994
Bastien Gili-tos <bast.gilitos@gmail.com>
2018-09-26 17:15:48 +0200

    create urlencoded parser from bodyParser

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = require('express')();
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 
+
 /* Use session middleware */
 app.use(cookieSession({ secret: 'secretkey' }));
+
+
+/* Create application/x-www-form-urlencoded parser */
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 app.get('/', (req, res) => res.render('index.pug'));

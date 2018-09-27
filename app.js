925689e53639d97a1cb63af04c53baec3dac6d5f
Bastien Gili-tos <bast.gilitos@gmail.com>
2018-09-27 10:06:15 +0200

    Split todolist creation in a different middleware

--- app.js
+++ app.js
@@ -3,27 +3,38 @@ const app = require('express')();
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 
-
-/* Use session middleware */
-app.use(cookieSession({ secret: 'secretkey' }));
-
-
 /* Use pug as template engine */
 app.set('view engine', 'pug');
-
 
 /* Create application/x-www-form-urlencoded parser */
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
-/* Create todolist if dealing with new session,
-   render index.pug with todolist as local variable in both cases */
-app.get('/todo', (req, res) => {
+/* Use session middleware */
+app.use(cookieSession({ secret: 'secretkey' }));
+
+
+/* Create todolist array if dealing with new session
+   (middleware executed every time the app receive a request) */
+app.use((req, res, next) => {
   if (!req.session.todolist) {
     req.session.todolist = [];
   }
+  next();
+});
 
+
+/* Render index.pug with todolist as local variable */
+app.get('/todo', (req, res) => {
   res.render('index', { todolist: req.session.todolist });
+});
+
+
+/* Add a todo to list */
+app.post('/todo/add', urlencodedParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  res.send('response :' + req.body.todo);
 });
 
 
